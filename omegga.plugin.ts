8cd74a02859937bdb8ced8ff7516783730441266
Dante Stevens <danteorionstevens@gmail.com>
2022-06-17 13:40:22 -0400

    pushing current changes

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -26,18 +26,22 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
   msPerDayHour = this.msPerHour * 2; //hours between 6am and 6pm are twice as long
   msPerDay = (12 * this.msPerHour) + (12 * this.msPerDayHour); //27000000 45min //game day
 
-  startSunrise = 5;
+  startSunrise = 4.4;
   sunrise = 6;
-  endSunrise = 6.5;
-  startSunset = 17.5;
+  endSunrise = 6.1;
+  startSunset = 17.1;
   sunset = 18;
-  endSunset = 19;
+  endSunset = 18.6;
 
   async init() {
-    this.getEnv();
-
     // Write your plugin!
     this.omegga.on('cmd:starthelper', async (speaker: string, ...args: string[]) => {
+      Omegga.loadEnvironmentData({data:{groups:{Sky:{
+        timeChangeSpeed: 0, 
+        skyIntensity: this.nightSkyIntensity, 
+        timeOfDay: this.startSunrise-.5}}}})
+      await this.getEnv();
+
       this.stopHelperTimeout();
       if(args.length){
         let multiplier = parseInt(args[0]);
@@ -75,6 +79,7 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
           } else { //transition period
             //start env transition
             console.log('transition');
+            this.saveCurrentSky();
             nextTick = (this.twoHoursToMs(currTime, currTime < 12 ? this.endSunrise : this.endSunset)) + 10
             this.startTransition(this.env.data.groups.Sky.skyIntensity, currTime < 12 ? this.daySkyIntensity : this.nightSkyIntensity, (nextTick / speed) - 20);
           }
@@ -109,14 +114,16 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
       this.stopTransitionTimeout();
     }
     if(starti != stopi){
-      this.transitionTick(starti, (stopi - starti), 0.0, transitionTime, transitionTime/100);
+      let transitionTickRate = transitionTime/50; //50 subdivisions
+      console.log(`transition tickrate: ${transitionTickRate}`);
+      this.transitionTick(starti, (stopi - starti), 0.0, transitionTime, transitionTickRate);
     }
   }
 
   transitionTick = async (starti: number, stopi: number, x: number, transitionTime: number, tickRate: number) => {
     let intensity:number;
     if(x < transitionTime){
-    intensity = (((Math.atan((x/transitionTime - 0.5)*10)*0.365)+0.5) * stopi) + starti;
+    intensity = (((Math.atan((Math.pow(x/transitionTime, 2) - 0.5)*6)*0.4)+0.5) * stopi) + starti;
     this.transitionTimeout = setTimeout(async () => {await this.transitionTick(starti, stopi, x + tickRate, transitionTime, tickRate);}, tickRate); 
     } else {
       intensity = (stopi + starti);
@@ -127,7 +134,14 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
   }
 
   getEnv = async () => {
+    console.log("getting env")
+    let start = new Date();
     this.env = await Omegga.getEnvironmentData();
+    let end = new Date();
+    console.log(`finished getting env in ${end.getTime() - start.getTime()}ms`)
+  }
+
+  saveCurrentSky = () => {
     if(this.isDayPeak()){
       this.daySkyIntensity = this.env.data.groups.Sky.skyIntensity;
       console.log(`Saved day intensity as ${this.daySkyIntensity}`);
@@ -171,7 +185,7 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
     if(this.helperTimeout){
       clearTimeout(this.helperTimeout);
       this.helperTimeout = 0;
-      this.omegga.broadcast(`daynighthelper stopped.`);
+      console.log(`daynighthelper stopped.`);
     }
   }
 
@@ -179,11 +193,13 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
     if(this.transitionTimeout){
       clearTimeout(this.transitionTimeout);
       this.transitionTimeout = 0;
-      this.omegga.broadcast(`daynighthelper transition stopped.`);
+      console.log(`daynighthelper transition stopped.`);
     }
   }
 
   async stop() {
-    // Anything that needs to be cleaned up...
+    this.stopHelperTimeout();
+    this.stopTransitionTimeout();
+    console.log(`daynighthelper plugin stopped.`);
   }
 }
