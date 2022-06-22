3c563d6f5d02feaf0563b99477638d900d5ab2b7
Dante Stevens <danteorionstevens@gmail.com>
2022-06-21 20:52:25 -0400

    transition improvements

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -1,5 +1,6 @@
 import OmeggaPlugin, { OL, PS, PC } from 'omegga';
 import { EnvironmentPreset } from '../omegga/src/brickadia/presets';
+import { BRColor } from '../onion/omegga';
 
 type Config = { foo: string };
 type Storage = { bar: string };
@@ -19,30 +20,47 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
   transitionTimeout:number;
   env:EnvironmentPreset;
 
-  nightSkyIntensity = 7.5;
-  daySkyIntensity = 1;
+  nightSkyIntensity:number = 7.5;
+  nightSkyColor: BRColor  = {r: .133, g: 0.165, b: .376, a: 1};
+  daySkyIntensity:number = 1;
+  daySkyColor: BRColor = {r: 0, g: 0.171, b: 1, a: 1};
   
   msPerHour = 1000 * 75; //75000 1min15s //game hour
   msPerDayHour = this.msPerHour * 2; //hours between 6am and 6pm are twice as long
   msPerDay = (12 * this.msPerHour) + (12 * this.msPerDayHour); //27000000 45min //game day
 
-  startSunrise = 4.4;
+  startSunrise = 4.8;
   sunrise = 6;
-  endSunrise = 6.1;
+  endSunrise = 6.2;
   startSunset = 17.1;
   sunset = 18;
   endSunset = 18.6;
 
+  startSpeed = 4;
+  startBuffer = -.25;
+
   async init() {
-    // Write your plugin!
     this.omegga.on('cmd:starthelper', async (speaker: string, ...args: string[]) => {
+      startHelper(speaker,...args);
+    });
+
+    this.omegga.on('cmd:stophelper', (speaker: string) => {
+      this.stopHelperTimeout();
+      this.stopTransitionTimeout();
+      Omegga.loadEnvironmentData({data:{groups:{Sky:{timeChangeSpeed: 0}}}})
+    })
+
+    const startHelper = async(speaker: string, ...args: string[]) => {
+      /*
       Omegga.loadEnvironmentData({data:{groups:{Sky:{
         timeChangeSpeed: 0, 
-        skyIntensity: this.nightSkyIntensity, 
-        timeOfDay: this.startSunrise-.5}}}})
+        skyIntensity: this.daySkyIntensity, 
+        skyColor: this.daySkyColor,
+        timeOfDay: this.startSunset+this.startBuffer}}}})*/
       await this.getEnv();
 
       this.stopHelperTimeout();
+      this.stopTransitionTimeout();
       if(args.length){
         let multiplier = parseInt(args[0]);
         if(!isNaN(multiplier)){
@@ -56,14 +74,7 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
         console.log(`daynighthelper started by ${speaker} at ${this.env.data.groups.Sky.timeChangeSpeed}x speed`);
       }
       await helperTick();
-    });
-
-    this.omegga.on('cmd:stophelper', (speaker: string) => {
-      console.log(`Stopped daynighthelper.`);
-      this.stopHelperTimeout();
-      this.stopTransitionTimeout();
-      Omegga.loadEnvironmentData({data:{groups:{Sky:{timeChangeSpeed: 0}}}})
-    })
+    }
 
     const helperTick = async () => {
       await this.getEnv();
@@ -81,7 +92,11 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
             console.log('transition');
             this.saveCurrentSky();
             nextTick = (this.twoHoursToMs(currTime, currTime < 12 ? this.endSunrise : this.endSunset)) + 10
-            this.startTransition(this.env.data.groups.Sky.skyIntensity, currTime < 12 ? this.daySkyIntensity : this.nightSkyIntensity, (nextTick / speed) - 20);
+            this.startTransition(this.env.data.groups.Sky.skyIntensity, 
+              currTime < 12 ? this.daySkyIntensity : this.nightSkyIntensity,
+              this.env.data.groups.Sky.skyColor,
+              currTime < 12 ? this.daySkyColor : this.nightSkyColor,
+              (nextTick / speed) - 20);
           }
         } else { //night
           console.log('night');
@@ -104,55 +119,73 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
       }
     }
 
+    //startHelper('orion',this.startSpeed.toString());
+
     return { registeredCommands: ['starthelper', 'stophelper'] };
 
   }
 
-  startTransition = async (starti: number, stopi: number, transitionTime: number) => {
+  startTransition = async (starti: number, stopi: number, startc: BRColor, stopc: BRColor, transitionTime: number) => {
     console.log(`Started a transition from ${starti} to ${stopi} over ${transitionTime}ms.`);
+    //this.omegga.broadcast(`Started a transition from ${starti} to ${stopi} over ${transitionTime}ms.`);
     if(this.transitionTimeout){
       this.stopTransitionTimeout();
     }
     if(starti != stopi){
-      let transitionTickRate = transitionTime/50; //50 subdivisions
+      let transitionTickRate = transitionTime/200; //50 subdivisions
       console.log(`transition tickrate: ${transitionTickRate}`);
-      this.transitionTick(starti, (stopi - starti), 0.0, transitionTime, transitionTickRate);
+      this.transitionTick(starti, (stopi - starti), startc, stopc, 0.0, transitionTime, transitionTickRate);
     }
   }
 
-  transitionTick = async (starti: number, stopi: number, x: number, transitionTime: number, tickRate: number) => {
+  transitionTick = async (starti: number, stopi: number, startc: BRColor, stopc: BRColor, x: number, transitionTime: number, tickRate: number) => {
     let intensity:number;
+    let skyColor:BRColor;
     if(x < transitionTime){
-    intensity = (((Math.atan((Math.pow(x/transitionTime, 2) - 0.5)*6)*0.4)+0.5) * stopi) + starti;
-    this.transitionTimeout = setTimeout(async () => {await this.transitionTick(starti, stopi, x + tickRate, transitionTime, tickRate);}, tickRate); 
+    let intensityStep = this.intensityFunc(x/transitionTime);
+    intensity = (intensityStep * stopi) + starti;
+    skyColor = {
+      r: (intensityStep * (stopc.r - startc.r)) + startc.r, 
+      g: (intensityStep * (stopc.g - startc.g)) + startc.g, 
+      b: (intensityStep * (stopc.b - startc.b)) + startc.b, 
+      a: 1}
+    this.transitionTimeout = setTimeout(async () => {await this.transitionTick(starti, stopi, startc, stopc, x + tickRate, transitionTime, tickRate);}, tickRate); 
     } else {
       intensity = (stopi + starti);
+      skyColor = {
+        r: stopc.r, 
+        g: stopc.g, 
+        b: stopc.b, 
+        a: 1}
       console.log(`Day/night transition finished at sky intensity ${intensity}`);
+      //this.omegga.broadcast(`Day/night transition finished at sky intensity ${intensity}`);
       this.transitionTimeout = 0;
     }
-    this.loadEnvData(intensity);
+    this.loadEnvData(intensity, skyColor);
+  }
+
+  intensityFunc = (x: number):number => {
+    return ((Math.atan((Math.pow(x, 2) - 0.5)*6)*0.4)+0.5)
   }
 
   getEnv = async () => {
-    console.log("getting env")
-    let start = new Date();
     this.env = await Omegga.getEnvironmentData();
-    let end = new Date();
-    console.log(`finished getting env in ${end.getTime() - start.getTime()}ms`)
   }
 
   saveCurrentSky = () => {
     if(this.isDayPeak()){
       this.daySkyIntensity = this.env.data.groups.Sky.skyIntensity;
-      console.log(`Saved day intensity as ${this.daySkyIntensity}`);
+      this.daySkyColor = this.env.data.groups.Sky.skyColor;
+      console.log(`Saved daytime intensity (${this.daySkyIntensity}) and color. (r${this.daySkyColor.r},g${this.daySkyColor.g},b${this.daySkyColor.b})`);
     } else if (this.isNight()){
       this.nightSkyIntensity = this.env.data.groups.Sky.skyIntensity;
-      console.log(`Saved night intensity as ${this.nightSkyIntensity}`);
+      this.nightSkyColor = this.env.data.groups.Sky.skyColor;
+      console.log(`Saved nighttime intensity (${this.nightSkyIntensity}) and color. (r${this.nightSkyColor.r},g${this.nightSkyColor.g},b${this.nightSkyColor.b})`);
     }
   }
 
-  loadEnvData = (intensity: number) => {
-    Omegga.loadEnvironmentData({data:{groups:{Sky:{skyIntensity: intensity}}}});
+  loadEnvData = (intensity: number, skyColor: BRColor) => {
+    Omegga.loadEnvironmentData({data:{groups:{Sky:{skyIntensity: intensity, skyColor: skyColor}}}});
   }
 
   isDayPeak = ():boolean => {
